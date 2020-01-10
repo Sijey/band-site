951b6d2063cff0fbc974c1bb79bb84f8cb795903
Sijey <popov040391@gmail.com>
2020-01-10 10:50:46 +0200

    first

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+[Demo Link](https://sijey.github.io/band-site/).
+
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import AudioPlayer from 'react-h5-audio-player';
+import ReactAudioPlayer from 'react-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
 
 function App() {
   return (
-    <AudioPlayer
-      src='in_my_eyes.mp3'
+    <ReactAudioPlayer
+      src='https://github.com/Sijey/band-site/tree/master/public/audio/in_my_eyes.mp3'
       autoPlay
       controls
     />
